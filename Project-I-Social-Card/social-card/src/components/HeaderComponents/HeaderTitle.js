330d279cdd7593fed4ce8c3fbc5569c3a191cd01
JacobWellner83 <jcwellner83@gmail>
2019-05-08 20:50:29 -0500

    completed HeaderTitle including export

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="title-container">
+            <p className="first">Lambda School</p>
+            <p className="second">
+              <span className="@user">@LambdaSchool</span>{` `}&bull;{` `}
+              <span className="date">26 Jan</span>  
+            </p>
+        </div>
+    )
+}
+
+export default HeaderTitle
