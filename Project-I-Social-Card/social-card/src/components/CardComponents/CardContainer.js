491cf8ae9412d72db8d448510d16d5a23290f2b9
JacobWellner83 <jcwellner83@gmail>
2019-05-08 21:26:04 -0500

    completed CardContainer and CardContent including export

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,7 +6,14 @@ import CardContent from './CardContent'
 
 const CardContainer = () => {
     return (
-        
+        <div className="card-wrap">
+            <div className="banner-container">
+                <CardBanner />
+            </div>
+            <div className="card-content-container">
+                <CardContent />
+            </div>
+        </div>
     )
 }
 
