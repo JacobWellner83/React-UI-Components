0451a1ed3d8707df3770cbb90c7e5e971d2f0102
JacobWellner83 <jcwellner83@gmail>
2019-05-08 21:10:45 -0500

    completed CardBanner including export

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner'
+import CardContent from './CardContent'
+
+const CardContainer = () => {
+    return (
+        
+    )
+}
+
+export default CardContainer
