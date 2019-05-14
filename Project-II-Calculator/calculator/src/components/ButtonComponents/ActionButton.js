105b507583c218b4b4d4d74c227ac0800854a90a
JacobWellner83 <jcwellner83@gmail>
2019-05-13 20:09:16 -0500

    App.js corrected with proper imports, NumberButton and ActionButton Props

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const ActionButton = props => {
     return (
-        <button className={``}>
+        <button className={`three-quarter-button ${props.className}`}>
             {props.text}
         </button>
     );
