e36c550aac06445e8afe17a59497bc1f604c2b0f
JacobWellner83 <jcwellner83@gmail>
2019-05-13 19:50:46 -0500

    App.js outlined

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -11,7 +11,30 @@ const App extends React.Component {
       <div className="calc-Container">
         <CalculatorDisplay />>
         <ButtonContainer className="clear-button">
-          <ActionButton className=""
+          <ActionButton className="triple-text" text="Clear" />
+          <NumberButton bgColor="red-button" text="/" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="num-button" text="7" />
+          <NumberButton bgColor="num-button" text="8" />
+          <NumberButton bgColor="num-button" text="9" />
+          <NumberButton bgColor="red-button" text="X" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="num-button" text="4" />
+          <NumberButton bgColor="num-button" text="5" />
+          <NumberButton bgColor="num-button" text="6" />
+          <NumberButton bgColor="red-button" text="-" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="num-button" text="1" />
+          <NumberButton bgColor="num-button" text="2" />
+          <NumberButton bgColor="num-button" text="3" />
+          <NumberButton bgColor="red-button" text="+" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <ActionButton className="triple-text" text="0" />
+          <NumberButton bgColor="red-button" text="=" />
         </ButtonContainer>
       </div>
     );
